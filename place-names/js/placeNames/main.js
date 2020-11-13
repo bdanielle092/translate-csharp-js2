c743040788260c876f4bb9ca889e67a56d5325d5
Faith Magras <bdanielle092@gmail.com>
2020-11-13 14:01:59 -0600

    finish place-name

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,19 @@
 function main() {
   // Put your code here
+  console.log("All Place Names");
+  var names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"];
+  for (let name of names) {
+    console.log(name)
+  }
+
+  console.log("----------");
+  console.log("'The' Place Names");
+  var theNames = names
+  for (name of theNames) {
+    if (name.startsWith("The") === true)
+      console.log(name)
+  }
 
 }
-
 main();
