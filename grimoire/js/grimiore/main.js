7d65e99e6079fd02e36ac4c0fc7e2dd0782f74c0
Faith Magras <bdanielle092@gmail.com>
2020-11-18 19:25:34 -0600

    working on word-frequency

--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -1,5 +1,17 @@
 function main() {
   // Put your code here
+  console.log("Do you believe in magic?")
+  console.log("------------------------")
+  const allSpells = []
+  const goodBook = makeGoodSpellBook(allSpells)
+  const evilBook = makeEvilSpellBook(allSpells)
+
+  function displaySpellBook(spellBook, book) {
+    console.log(book.Title);
+    for (spell in spell.book) {
+      console.log(spell.Name);
+    }
+  }
 
 }
 
